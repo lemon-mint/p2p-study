78622b24aeeac8980c6174db7d7bed19f7e5e3e5
lemon-mint <ice.lemons.mint@gmail.com>
2022-05-06 22:24:15 +0900

    Add godotenv

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lemon-mint/p2p-study
 
 go 1.18
+
+require github.com/lemon-mint/godotenv v1.0.3
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "github.com/lemon-mint/godotenv"
 
+func main() {
+	godotenv.Load()
 }
